64c7630ec7005e3e69ecbd8cb7fcb4ce3a0696e8
Antoine Falisse <antoinefalisse@gmail.com>
2018-09-10 14:37:13 +0200

    add methods to header

--- recorder.hpp
+++ recorder.hpp
@@ -21,6 +21,83 @@ public:
     std::ostream& operator<<(std::ostream &stream, const Recorder& obj);
   static void start_recording();
   static void stop_recording();
+  
+  /* Assignments */
+  double getValue() const;
+  inline double value() const;
+  Recorder& operator = ( double );
+  Recorder& operator = ( const Recorder& );
+  
+  /* IO friends */
+  inline friend 
+    std::istream& operator >> (std::istream& is, const Recorder& a);
+	
+  /* Operation + Assignment */
+  Recorder& operator += ( double );
+  Recorder& operator += ( const Recorder& );
+  Recorder& operator -= ( double y );
+  Recorder& operator -= ( const Recorder& );
+  Recorder& operator *= ( double );
+  Recorder& operator *= ( const Recorder& );
+  Recorder& operator /= ( double );
+  Recorder& operator /= ( const Recorder& );
+  
+  /* Comparison (friends) */
+  inline friend 
+    int operator != ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator == ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator <= ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator >= ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator >  ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator <  ( const Recorder&, const Recorder& );
+  inline friend 
+    int operator != (double, const Recorder& rhs);
+  inline friend 
+    int operator == ( double, const Recorder& rhs);
+  inline friend 
+    int operator <= ( double, const Recorder& rhs);
+  inline friend 
+    int operator >= ( double, const Recorder& rhs);
+  inline friend 
+    int operator >  ( double, const Recorder& rhs);
+  inline friend 
+    int operator <  ( double, const Recorder& rhs);
+	
+  /* sign operators (friends) */
+  friend Recorder operator + ( const Recorder& x );
+  friend Recorder operator - ( const Recorder& x );
+  
+  /* binary operators (friends) */
+    friend Recorder operator + ( const Recorder&, const Recorder& );
+    friend Recorder operator + ( double, const Recorder& );
+    friend Recorder operator + ( const Recorder&, double );
+    friend Recorder operator - ( const Recorder&, const Recorder& );
+    inline friend Recorder operator - ( const Recorder&, double );
+    friend Recorder operator - ( double, const Recorder& );
+    friend Recorder operator * ( const Recorder&, const Recorder& );
+    friend Recorder operator * ( double, const Recorder& );
+    inline friend Recorder operator * ( const Recorder&, double );
+    inline friend Recorder operator / ( const Recorder&, double );
+    friend Recorder operator / ( const Recorder&, const Recorder& );
+    friend Recorder operator / ( double, const Recorder& );
+	
+	 /* unary operators (friends) */
+    friend Recorder exp  ( const Recorder& );
+    friend Recorder log  ( const Recorder& );
+    friend Recorder sqrt ( const Recorder& );
+    friend Recorder sin  ( const Recorder& );
+    friend Recorder cos  ( const Recorder& );
+    friend Recorder tan  ( const Recorder& );
+    friend Recorder asin ( const Recorder& );
+    friend Recorder acos ( const Recorder& );
+    friend Recorder atan ( const Recorder& );
+	
+  
    
 protected:
   void disp(std::ostream &stream) const;

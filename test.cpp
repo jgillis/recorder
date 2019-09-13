faf064242bb5a9b79b94180031907932071a00d1
Antoine Falisse <antoinefalisse@gmail.com>
2019-09-13 09:34:26 +0200

    test

--- test.cpp
+++ test.cpp
@@ -2,53 +2,74 @@
 
 int main(){
 
-  double p = 7;
+  Recorder a;
+  Recorder b;
 
-  Recorder x;
-  Recorder y;
-  Recorder t;
+  a <<= 2;
+  b <<= 3;
 
-  x <<= 2;
-  y <<= 5.5;
-  t <<= 6+1.0/3;
+  Recorder z = a*b;
+  Recorder y1 = cos(a)-z;
 
-  std::cout << "t:" << t << std::endl;
-  t = 5;
-  std::cout << "t:" << t << std::endl;
-  t = t+3;
-  std::cout << "t:" << t << std::endl;
-
-  std::cout << "x:" << x << std::endl;
-  x = x+3;
-  std::cout << "x:" << x << std::endl;
-  x = x+3;
-  std::cout << "x:" << x << std::endl;
-  x = x+t;
-  std::cout << "x:" << x << std::endl;
-
-  t = x;
-  std::cout << "t:" << t << std::endl;
-  t = 99;
-  std::cout << "t:" << t << std::endl;
-  Recorder z = x+y;
-
-  if (z>=20) {
-    z = z+50;
-  } else {
-    z = z+3;
-  }
-
-
-  z = z*(x>=0);
-  z = sin(z);
-  std::cout << "z:" << z << std::endl;
-
-  double znum;
-  z >>= znum;
+  double y1num;
+  y1 >>= y1num;
 
  Recorder::stop_recording();
 
-  std::cout << znum << std::endl;
-
   return 0;
 }
+
+//#include "recorder.hpp"
+//
+//int main(){
+//
+//  double p = 7;
+//
+//  Recorder x;
+//  Recorder y;
+//  Recorder t;
+//
+//  x <<= 2;
+//  y <<= 5.5;
+//  t <<= 6+1.0/3;
+//
+//  std::cout << "t:" << t << std::endl;
+//  t = 5;
+//  std::cout << "t:" << t << std::endl;
+//  t = t+3;
+//  std::cout << "t:" << t << std::endl;
+//
+//  std::cout << "x:" << x << std::endl;
+//  x = x+3;
+//  std::cout << "x:" << x << std::endl;
+//  x = x+3;
+//  std::cout << "x:" << x << std::endl;
+//  x = x+t;
+//  std::cout << "x:" << x << std::endl;
+//
+//  t = x;
+//  std::cout << "t:" << t << std::endl;
+//  t = 99;
+//  std::cout << "t:" << t << std::endl;
+//  Recorder z = x+y;
+//
+//  if (z>=20) {
+//    z = z+50;
+//  } else {
+//    z = z+3;
+//  }
+//
+//
+//  z = z*(x>=0);
+//  z = sin(z);
+//  std::cout << "z:" << z << std::endl;
+//
+//  double znum;
+//  z >>= znum;
+//
+// Recorder::stop_recording();
+//
+//  std::cout << znum << std::endl;
+//
+//  return 0;
+//}
